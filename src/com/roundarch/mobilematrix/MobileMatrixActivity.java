552f668f1acb469166b4013ba2a70601372d5fb3
Aidan Coyne <raptros.v76@gmail.com>
2010-06-11 10:16:18 -0400

    initialized android project

---
+++ src/com/roundarch/mobilematrix/MobileMatrixActivity.java
@@ -0,0 +1,15 @@
+package com.roundarch.mobilematrix;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class MobileMatrixActivity extends Activity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState)
+    {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.main);
+    }
+}
